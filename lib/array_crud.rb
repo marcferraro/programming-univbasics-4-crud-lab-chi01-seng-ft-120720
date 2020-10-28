5c70dfe7fe53f5b6e3c6b6fe6029d4f4094c71e2
Marc Ferraro <marc.ferraro1@gmail.com>
2020-10-28 19:43:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["sun", "moon", "fire", "dark"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = array.unshift
 end
 
 def remove_element_from_end_of_array(array)
